7e2c2bbb8f888b37995e434993ff015a3629e736
Akshit Thakur <akshit2649@gmail.com>
2023-01-05 01:35:02 +0530

    graphql schema

---
+++ backend/src/graphql/typeDefs/index.ts
@@ -0,0 +1,7 @@
+import userTypeDefs from "./user";
+// import conversationTypeDefs from "./conversation";
+// import messageTypeDefs from "./message";
+
+const typeDefs = [userTypeDefs];
+
+export default typeDefs;
