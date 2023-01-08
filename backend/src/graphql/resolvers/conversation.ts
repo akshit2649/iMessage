ced3b7a1e261eac2de9398f5c0862176d400bf1b
Akshit Thakur <akshit2649@gmail.com>
2023-01-09 00:51:16 +0530

    create conversation resolver

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -1,4 +1,6 @@
 import { GraphQLContext } from "../../util/types";
+import { Prisma } from "@prisma/client";
+import { ApolloError } from "apollo-server-core";
 
 const resolvers = {
   Mutation: {
@@ -7,9 +9,64 @@ const resolvers = {
       args: { participantsIds: Array<string> },
       context: GraphQLContext
     ) => {
-      console.log("INSIDE CREATE CONVERSATION", args);
+      const { session, prisma } = context;
+      const { participantsIds } = args;
+
+      if (!session?.user) {
+        throw new ApolloError("Not Authorized");
+      }
+
+      const {
+        user: { id: userId },
+      } = session;
+
+      try {
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantsIds.map((id) => ({
+                  userId: id,
+                  hasSeenLatestMessage: id === userId,
+                })),
+              },
+            },
+          },
+          include: conversationPopulated,
+        });
+      } catch (error: any) {
+        console.log("CreateConversation Error", error);
+        throw new ApolloError("Error creating conversation");
+      }
     },
   },
 };
+
+export const participantPopulated =
+  Prisma.validator<Prisma.ConversationParticipantInclude>()({
+    user: {
+      select: {
+        id: true,
+        username: true,
+      },
+    },
+  });
+
+export const conversationPopulated =
+  Prisma.validator<Prisma.ConversationInclude>()({
+    participants: {
+      include: participantPopulated,
+    },
+    latestMessage: {
+      include: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+    },
+  });
 
 export default resolvers;
